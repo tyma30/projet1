4d41c65364cc3a4d70e6beab7561e154dcc6e7eb
tyma30 <59802634+tyma30@users.noreply.github.com>
2020-11-30 18:22:43 +0000

    ok

--- src/main/java/Calcul/Main.java
+++ src/main/java/Calcul/Main.java
@@ -2,7 +2,7 @@ package Calcul;
 
 public class Main {
     public static void  main(String[] agrs){
-        System.out.println("Bonjour à toute la classe.");
-        System.out.print("Il fait 18H ");
+        System.out.println("projets genie logiciel");
+
     }
 }
